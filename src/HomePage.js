95b047ed3839b45cc5bf8284babc0591fa473844
vinny <vinnygeoffrey@gmail.com>
2021-12-05 12:12:51 -0600

    fourth commit

---
+++ src/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+ 
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS? </h2>
+        <p>The easiest thing to do is post on our<a href="forums">forums</a></p>
+ 
+       </div>
+    );
+  }
+}
+export default HomePage;
